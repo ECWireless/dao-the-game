53840009a5649e664331da22ea4c9ef72bec521d
ECWireless <40322776+ECWireless@users.noreply.github.com>
2026-03-04 11:07:28 -0700

    feat: add Vercel Analytics (#6)

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -8,5 +9,6 @@ import './theme/global.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
